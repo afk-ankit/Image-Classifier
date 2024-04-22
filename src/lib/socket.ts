f0b2cd9f95d82b58b175a89532fb51b8259123e1
Ankit <ankitsharmgh093@gmail.com>
2024-04-22 13:08:34 +0530

    changes in socket.ts

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
+const devMode = import.meta.env.DEV;
 
-const URL = "http://localhost:8000";
+const URL = devMode
+  ? "http://localhost:8000"
+  : import.meta.env.VITE_BACKEND_URL;
 
 export const socket = io(URL);
